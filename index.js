007722934b3b2dd2f16091467ecd664e0f477eac
Ariana <ariana.morin214@gmail.com>
2023-12-31 11:30:00 -0600

    JS added for date and time.

---
+++ index.js
@@ -0,0 +1,21 @@
+function updateTime() {
+  let londonElement = document.querySelector("#london");
+  let londonClockElement = londonElement.querySelector(".time");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTime = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  londonClockElement.innerHTML = londonTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
+
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoClockElement = tokyoElement.querySelector(".time");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  tokyoClockElement.innerHTML = tokyoTime.format("h:mm:ss[<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
