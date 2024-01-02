b452933ff253be67c8697f93a78ae6cb5cc4b247
Ariana <ariana.morin214@gmail.com>
2024-01-02 12:40:15 -0600

    JS added for drop-down to be placed into h2.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateTime() {
 
   londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
   londonClockElement.innerHTML = londonTime.format(
-    "h:mm:ss[<small>]A[</small>]"
+    "h:mm:ss  [<small>]A[</small>]"
   );
 
   let tokyoElement = document.querySelector("#tokyo");
@@ -15,15 +15,35 @@ function updateTime() {
   let tokyoTime = moment().tz("Asia/Tokyo");
 
   tokyoDateElement.innerHTML = moment().format("MMMM Do YYYY");
-  tokyoClockElement.innerHTML = tokyoTime.format("h:mm:ss[<small>]A[</small>]");
+  tokyoClockElement.innerHTML = tokyoTime.format(
+    "h:mm:ss  [<small>]A[</small>]"
+  );
 
   let limaElement = document.querySelector("#lima");
   let limaClockElement = limaElement.querySelector(".time");
-  let limaDateElement = londonElement.querySelector(".date");
+  let limaDateElement = limaElement.querySelector(".date");
   let limaTime = moment().tz("America/Lima");
 
   limaDateElement.innerHTML = moment().format("MMMM Do YYYY");
-  limaClockElement.innerHTML = limaTime.format("h:mm:ss[<small>]A[</small>]");
+  limaClockElement.innerHTML = limaTime.format("h:mm:ss  [<small>]A[</small>]");
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let chooseTimeZone = event.target.value;
+  let chooseTime = moment().tz(chooseTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = chooseTimeZone;
+  citiesElement = `<h2 class="cities">
+        <div class="box-city" id="london">
+          <div class="current-city">${chooseTimeZone}</div>
+          <div class="time">${chooseTime.format(
+            "h:mm:ss"
+          )}</div><div class="am-pm"></div>
+             <div class="date">${chooseTime.format("MMMM Do YYYY")}</div>
+        </div>`;
+}
+
+let citiesSelectElement = document.querySelector("#choose");
+citiesSelectElement.addEventListener("change", updateCity);
